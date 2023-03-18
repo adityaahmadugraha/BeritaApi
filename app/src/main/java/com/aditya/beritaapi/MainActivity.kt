ab6cbc86ca7f99d7ee118208c0a1f654259da93b
Aditya ahmad nugraha <adityaahmadnugraha091@gmail.com>
2023-03-18 22:33:16 +0700

    update

--- app/src/main/java/com/aditya/beritaapi/MainActivity.kt
+++ app/src/main/java/com/aditya/beritaapi/MainActivity.kt
@@ -2,10 +2,16 @@ package com.aditya.beritaapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aditya.beritaapi.databinding.ActivityDetailBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
